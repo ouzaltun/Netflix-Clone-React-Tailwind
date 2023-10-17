e5565176ceb01cb207eb2cbc4b62d98a591803ad
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-10-17 23:13:51 +0300

    Netflix-Clone-React-Tailwind
    Netflix-Clone-React-Tailwind packageların kurulumu

---
+++ Netflix-Clone-React-Tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-500 flex justify-center items-center text-3xl ">
+        Netflix Clone
+      </div>
+    </>
+  );
+}
+
+export default App;
