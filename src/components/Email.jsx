4892babf335920ef6a3a2a4e6352fd34d94d1ce1
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-11-05 10:43:02 +0300

    Button fonksiyonu Email Focus
    Button fonksiyonu Email Focus

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useRef } from "react";
 import { IoIosArrowForward } from "react-icons/io";
 
 function Email() {
+  const inputRef = useRef();
+  const FocusInput = () => {
+    inputRef.current.focus();
+  };
   return (
     <div>
       <div className="flex flex-wrap items-center justify-center text-left mt-4 gap-y-4">
         <div className="relative flex items-center justify-center">
           <input
+            ref={inputRef}
             type="text"
             id="floating_filled"
             className="block min-h-[38px] md:min-h-[56px] max-w-[320px] md:max-w-[404px] md:min-w-[403px] text-white leading-6 bg-[rgba(0,0,0,0.29)] border pl-6 pt-4 pb-2 border-gray-400 rounded peer"
@@ -21,7 +27,10 @@ function Email() {
         </div>
 
         <div className="ml-2   ">
-          <button className=" border 0.5rem 1rem px-4 md:px-6 border-black bg-netflix text-white text-lg md:text-2xl cursor-pointer  tracking-[-0.4px] rounded font-bold transition-colors hover:bg-[#c11119]">
+          <button
+            onClick={FocusInput}
+            className=" border 0.5rem 1rem px-4 md:px-6 border-black bg-netflix text-white text-lg md:text-2xl cursor-pointer  tracking-[-0.4px] rounded font-bold transition-colors hover:bg-[#c11119]"
+          >
             <div className=" flex justify-center items-center h-[48px] md:h-[56px] md:pb-1">
               <span>Başlayın</span>
 
